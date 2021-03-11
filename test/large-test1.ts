e1e6707dbc2fbd04e36c8b3b5a36672dc02bbbb9
oliver021 <oliver021val@gmail.com>
2021-03-11 13:30:54 -0500

    add more test

--- test/large-test1.ts
+++ test/large-test1.ts
@@ -1,3 +1,81 @@
+import { from } from '../src';
+import { expect, assert } from 'chai';
 describe('large test about queries of the linqscript', () => {
-    it('1 - case => goal one record of seven');
+
+    const data = [
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1]
+        },
+        {
+            support: -111,
+            textMsg: "hi!!!",
+            range: [3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: [1,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "sys",
+            range: [1,1111,3211,11,391]
+        },
+        {
+            support: 1,
+            textMsg: "hi!!!",
+            range: []
+        },
+        {
+            support: 1,
+            textMsg: "null",
+            range: [1,1,391]
+        },
+    ];
+
+    it('1 - case => goal one record of seven', () =>{
+        const result = from(data).poll(x => x.support, false);
+        assert.isNotNull(result);
+    });
+
+    it('2 - simple exclusion', () =>{
+        const result = from(data).except(x => x.support < 0);
+        expect(result.count()).to.equal(data.length - 1);
+        assert.isFalse(result.contains(x => x.support < 0));
+        assert.isNull(result.single(x => x.support < 0));
+        assert.isNotNull(result.random());
+        assert.isNull(result.where(() => false).random());
+    });
 });
